f459eb419c13de47cafab6b2540030571fa23545
arthur01dev <arthurrodriguesdesouza66@gmail.com>
2024-06-26 16:54:05 -0300

    voltando versoes do meu projeto

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
-console.log("Aplicação se conectando com o banco de dadoa");
-// mais um console.log de teste: 
-console.log("O Paulo fez esse commit sem você ver na aula");
-console.log("Enviado para o GitHub")
