7dba4f60edf3352a582255cb5fd81f3185e70841
arthur01dev <arthurrodriguesdesouza66@gmail.com>
2024-06-26 16:45:45 -0300

    adicionando mais um console

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Aplicação se conectando com o banco de dadoa");
+// mais um console.log de teste: 
+console.log("O Paulo fez esse commit sem você ver na aula");
+console.log("Enviado para o github")
